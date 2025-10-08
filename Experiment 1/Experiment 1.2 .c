aebdb3861f3791970958fe06daf01c0f1d641522
siya09568-ux <siya09568@gmail.com>
2025-10-08 12:41:01 +0530

    Experiment 1.2 .c

--- Experiment 1/Experiment 1.2 .c
+++ Experiment 1/Experiment 1.2 .c
@@ -1,1 +1,16 @@
-
+/*Experiment 1: Installation, Environment Setup and starting with C 
+language 
+2. Write a C Program to print the address in multiple lines (new line).*/ 
+#include <stdio.h> 
+int main() 
+{ 
+printf("Name - Siya\nSAP ID - 590028348\nCourse
+BscComputerScience\nBatch-B1"); 
+printf("\n---------------------------------------------------------\n"); 
+printf("Name:Siya\n"); 
+printf("House no:161\n"); 
+printf("Street:Sanjay colony\n"); 
+printf("City:Bazpur\n"); 
+printf("Pincode:262401\n"); 
+return 0; 
+} 
