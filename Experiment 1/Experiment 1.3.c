a4512f037299bea5c3ab94b441d4a69879a073c1
siya09568-ux <siya09568@gmail.com>
2025-10-08 12:41:54 +0530

    Experiment 1.3.c

--- Experiment 1/Experiment 1.3.c
+++ Experiment 1/Experiment 1.3.c
@@ -1,1 +1,18 @@
-
+/*Experiment 1: Installation, Environment Setup and starting with C 
+language 
+3.Write a C program to add two numbers and take a number from the user.*/ 
+#include <stdio.h> 
+int main() 
+{ 
+int n1,n2,n3;  
+printf("Name - Siya\nSAP ID - 590028348\nCourse
+BscComputerScience\nBatch-B1"); 
+printf("\n---------------------------------------------------------\n"); 
+printf("enter the first number."); 
+scanf("%d",&n1);  
+printf("enter the second number.\n"); 
+scanf("%d",&n2);  
+n3=n1+n2;  
+printf("Sum=%d\n",n3);  
+return 0;
+}
