f2caf39ceab3e61cc203e3273a819f2330bbd48c
siya09568-ux <siya09568@gmail.com>
2025-10-08 12:40:12 +0530

    Experiment 1.1.c

--- Experiment 1/Experiment 1.1.c
+++ Experiment 1/Experiment 1.1.c
@@ -1,1 +1,12 @@
-
+/*Experiment 1: Installation, Environment Setup and starting with C 
+language 
+1. Write a C program to print “ Hello World”*/ 
+#include <stdio.h> 
+int main() 
+{ 
+printf("Name - Siya\nSAP ID - 590028348\nCourse 
+BscComputerScience\nBatch-B1"); 
+printf("\n---------------------------------------------------------\n"); 
+printf("Hello World"); 
+return 0;
+}
