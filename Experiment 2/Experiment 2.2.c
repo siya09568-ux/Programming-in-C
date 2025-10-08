302c572b8b7cc4ea4e4c25764ac72c348b45f25b
siya09568-ux <siya09568@gmail.com>
2025-10-08 12:49:17 +0530

    Create Experiment 2.2.c

---
+++ Experiment 2/Experiment 2.2.c
@@ -0,0 +1,17 @@
+/*Experiment 2: Operators 
+2.WAP a C program to Convert temperature from Celsius to 
+Fahrenheit using the formula: F=(C * 9/5)+32.*/ 
+#include <stdio.h> 
+int main() 
+{ 
+printf("Name - Siya\nSAP ID - 590028348\nCourse
+BscComputerScience\nBatch-B1"); 
+printf("\n---------------------------------------------------------\n"); 
+f
+ loat celsius,fahrenheit; 
+printf("enter the temperature in celsius: "); 
+scanf("%f", &celsius); 
+fahrenheit =(celsius * 9/5) +32; 
+printf("%f celsius = %f fahrenheit\n", celsius , fahrenheit);
+return 0; 
+}
