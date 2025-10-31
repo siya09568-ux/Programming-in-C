f3b518a91012126d3a9de5cd86e5b0e75c178ecd
siya09568-ux <siya09568@gmail.com>
2025-10-31 16:38:20 +0530

    Experiment 3.1.c

---
+++ Experiment 3/Experiment 3.1.c
@@ -0,0 +1,16 @@
+1.Write a C program to check whether a number is Even or ODD */ 
+#include <stdio.h> 
+int main() { 
+ printf("Name -Siya\nSAP ID - 590028348\n Course 
+ BscComputerScience\nBatch-B1"); 
+ printf("\n---------------------------------------------------------\n"); 
+int num; 
+ printf("Enter a number: "); 
+ scanf("%d", &num); 
+if (num % 2 == 0) { 
+ printf("%d is Even\n", num); 
+} else { 
+ printf("%d is Odd\n", num); 
+} 
+ return 0; 
+} 
